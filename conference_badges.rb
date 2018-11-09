9c20fe4f77d00eaac3d6db5288662064fd54fcff
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 21:17:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(names)
+  names.collect {|name| badge_maker(name)}
+end
+
 def assign_rooms(names)
   names.collect {|name| "#Hello, #{name}! You'll be assigned to room #{names.index(name)}!"}
 end
