234d1b2bd8ec2a5a5ec743e67000149b1f9f4645
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 21:26:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,8 @@ end
 def assign_rooms(names)
   names.collect {|name| "#Hello, #{name}! You'll be assigned to room #{names.index(name)}!"}
 end
+
+def printer(names)
+  names.each {|name| puts badge_maker(name)}
+  batch_badge_creator(names).each {|message| puts message}
+end
